518b6a1b297360429a15c43cac832d8ba2eaeb99
Md. Imran Hossain <mdimranhossain0713@gmail.com>
2024-09-23 15:43:28 +0600

    Add existing project files to Git

--- helpers/environment.js
+++ helpers/environment.js
@@ -14,8 +14,8 @@ environment.staging = {
     maxCheck : 5,
     twilio : {
         fromPhone : '+13319996145',
-        accountSid : 'ACbe8548bde4affed0dfb2170e582e9050',
-        authToken : '0672a2dac7d067b688ee913499168b8a'
+        accountSid : '***********************',
+        authToken : '************************'
     }
 };
 
@@ -26,8 +26,8 @@ environment.production = {
     maxCheck : 5,
     twilio : {
         fromPhone : '+13319996145',
-        accountSid : 'ACbe8548bde4affed0dfb2170e582e9050',
-        authToken : '0672a2dac7d067b688ee913499168b8a'
+        accountSid : '***********************',
+        authToken : '************************'
     }
 };
 
